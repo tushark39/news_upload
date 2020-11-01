3d9d0e66581d7000a00928a4fd09bbaffdd2a957
tushark39 <tushark39@gmail.com>
2020-11-01 17:45:13 +0530

    update https network

--- src/screen/UploadForm.js
+++ src/screen/UploadForm.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { Auth,Name } from '../helper/Auth';
+import { Auth, Name } from '../helper/Auth';
 import Login from './Login'
+import axios from 'axios'
+import { apiURL } from '../helper/API';
+
 // export const UploadForm = () => {
 
 // }   
 export default class UploadForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            news : ""
+        }
+    }
+    handleUpload = () =>{
+        axios.post(`${apiURL}/tasks`,{
+            "desc":this.state.news,
+            "completed":false
+        },{
+            headers: {
+                Authorization: 'Bearer ' + Auth //the token is a variable which holds the token
+              }
+        }).then(res=>console.log(res))
+        .catch(e=>console.log(e.message))
+    }
     render() {
         let isAuthenticated = Auth
-        let name =  Name
+        let name = Name
         return (
             <div>
                 {
@@ -15,12 +35,17 @@ export default class UploadForm extends React.Component {
                         <div className="upload-form">
                             <h3 className="section-title">Upload News</h3>
                             <p>Name of Intern : {name}</p>
-                            {/* Add a <textarea> tag */}
-                            <b><p><span style={{ color: "red" }}>*</span> Enter Article below  :- </p></b>
-                            <textarea defaultValue={""} />
+                    <b><p><span style={{ color: "red" }}>*</span> Enter Article below  :- {this.state.news}</p></b>
+                            <textarea defaultValue={""} value={this.state.news} onChange={e=>this.setState({news:e.target.value})} />
                             <p><span style={{ color: "red" }}>*</span> Required fields must be filled</p>
-                            {/* Add an <input> tag */}
-                            <input className="contact-submit" type="submit" defaultValue="Send" />
+                            <nav id="test" onClick={this.handleUpload}>
+                                <ul>
+                                    <li>
+                                        Upload
+                         <span></span><span></span><span></span><span></span>
+                                    </li>
+                                </ul>
+                            </nav>
                         </div>
                     )
                 }
