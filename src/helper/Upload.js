74a29fdc61b25404795343021be827346b1f12e7
tushark39 <tushark39@gmail.com>
2020-11-03 03:04:30 +0530

    Update at Uplaod Page

--- src/helper/Upload.js
+++ src/helper/Upload.js
@@ -6,8 +6,26 @@ export default class Upload extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            news: ""
+            news: "",
+            completed: 0,
+            pending: 0,
+            name: "",
+            heading: "Dummy",
+            Desc: "Dummmy",
+            data: []
         }
+    }
+    async componentDidMount() {
+        // axios.get("https://api.jsonbin.io/b/5fa06e8447077d298f5bdbdc")
+        // .then(res=>{
+        //     this.setState({name:res.data.name,Desc:res.data.Desc,heading:res.data.Heading})
+        // })
+        // .catch(e=>console.log("Error from Card ",e));
+        await axios.get("https://api.jsonbin.io/b/5fa06e8447077d298f5bdbdc")
+            .then(res => {
+                this.setState({ data: res.data })
+            })
+            .catch(e => console.log("Error from Data ", e))
     }
     handleUpload = () => {
         axios.post(`${apiURL}/tasks`, {
@@ -34,30 +52,37 @@ export default class Upload extends React.Component {
 
                     <div className="container">
                         <div className="row">
-                            <div  style={{ borderStyle: "solid",paddingTop:50,textAlign:"center" }} className="col-md-3">
+                            <div style={{ borderStyle: "solid", paddingTop: 50, textAlign: "center" }} className="col-md-3">
 
                                 <img src="https://image.shutterstock.com/image-vector/vector-design-avatar-dummy-sign-600w-1290556063.jpg" style={{ width: 200, height: 200, borderRadius: "50%", borderStyle: "solid" }} alt="" />
-                                <div >
+                                <div style={{ marginTop: 15 }} >
                                     <p>Name of Intern : {name}</p>
-                                    <div style={{textAlign:"center"}}>
+                                    <div style={{ textAlign: "center" }}>
                                         <div className="row">
                                             <div className="col">0</div>
                                             <div className="col">0</div>
                                         </div>
                                     </div>
-                                    <div style={{textAlign:"center"}}>
+                                    <div style={{ textAlign: "center" }}>
                                         <div className="row">
                                             <div className="col">Completed</div>
                                             <div className="col">Pending</div>
                                         </div>
                                     </div>
+                                    <div className="card text-white bg-primary mb-3" style={{ marginTop: 30 }}>
+                                        <div className="card-header">Your Current Task</div>
+                                        <div className="card-body">
+                                            <h4 className="card-title">{this.state.heading}</h4>
+                                            <p className="card-text">{this.state.Desc}</p>
+                                        </div>
+                                    </div>
                                 </div>
 
                             </div>
-                            <div  style={{ borderStyle: "solid",paddingTop:50,textAlign:"center" }} className="col-md-6">
+                            <div style={{ borderStyle: "solid", paddingTop: 50, textAlign: "center" }} className="col-md-6">
 
                                 <b><p><span style={{ color: "red" }}>*</span> Enter Article below  :- </p></b>
-                                <textarea defaultValue={""} value={this.state.news} onChange={e => this.setState({ news: e.target.value })} />
+                                <textarea rows="10" placeholder="Enter News Here" defaultValue={""} value={this.state.news} onChange={e => this.setState({ news: e.target.value })} />
                                 <p><span style={{ color: "red" }}>*</span> Required fields must be filled</p>
                                 <nav id="test" onClick={this.handleUpload}>
                                     <ul>
@@ -69,36 +94,11 @@ export default class Upload extends React.Component {
                                 </nav>
 
                             </div>
-                            <div  style={{ borderStyle: "solid",paddingTop:50,textAlign:"center" }} className="col-md-3">col</div>
-                        </div>
-                    </div>
+                            <div style={{ borderStyle: "solid", paddingTop: 50, textAlign: "center" }} className="col-md-3">
 
-
-                    {/* <div className="container">
-                        <div className="row">
-                            <div className="col">
-                                <img src="https://image.shutterstock.com/image-vector/vector-design-avatar-dummy-sign-600w-1290556063.jpg" style={{ width: 200, height: 200, borderRadius: "50%", borderStyle: "solid" }} alt="" />
-                                <div >
-                                    <p>Name of Intern : {name}</p>
-                                </div>
-                                <div className="col">
-                                    <b><p><span style={{ color: "red" }}>*</span> Enter Article below  :- </p></b>
-                                    <textarea defaultValue={""} value={this.state.news} onChange={e => this.setState({ news: e.target.value })} />
-                                    <p><span style={{ color: "red" }}>*</span> Required fields must be filled</p>
-                                    <nav id="test" onClick={this.handleUpload}>
-                                        <ul>
-                                            <li>
-                                                Upload
-                                                  <span></span><span></span><span></span><span></span>
-                                            </li>
-                                        </ul>
-                                    </nav>
-                                </div>
-                                <div className="col">hi</div>
                             </div>
                         </div>
                     </div>
-                */}
                 </div>
             </div>
         )
